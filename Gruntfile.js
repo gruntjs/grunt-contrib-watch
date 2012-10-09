80c33bc12e1bc6fa836ce9355113197be54fad4e
Kyle Robinson Young <kyle@dontkry.com>
2012-10-09 15:10:58 -0700

    Updates to run on Grunt v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,57 +7,51 @@
  * https://github.com/gruntjs/grunt-contrib-watch/blob/master/LICENSE-MIT
  */
 
+'use strict';
+
 module.exports = function(grunt) {
-  'use strict';
 
   // Project configuration.
   grunt.initConfig({
-    lint: {
-      all: [
-        'grunt.js',
-        'tasks/**/*.js',
-        '<config:nodeunit.tasks>'
-      ]
-    },
-
     jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
+        jshintrc: '.jshintrc'
       }
     },
 
     // Watch
     watch: {
       scripts: {
-        src: ['<config:lint.all>'],
+        src: ['<%= jshint.all %>'],
         tasks: ['lint', 'nodeunit']
       }
     },
 
     // Unit tests.
     nodeunit: {
-      tasks: ['test/tasks/**/*_test.js']
+      tests: ['test/**/*_test.js']
     }
+
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Rename test to nodeunit for forward compatibility
-  grunt.renameTask('test', 'nodeunit');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-internal');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['lint', 'test']);
+  grunt.registerTask('default', ['jshint', 'test']);
+
 };
