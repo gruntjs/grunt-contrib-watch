2584c177229f50f6625a96adec8505c3256f88de
Kyle Robinson Young <kyle@dontkry.com>
2012-10-09 15:13:17 -0700

    lint to jshint in watch config

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         src: ['<%= jshint.all %>'],
-        tasks: ['lint', 'nodeunit']
+        tasks: ['jshint', 'nodeunit']
       }
     },
 
