9d12703c8901e2f29668843c61b3ef096b37cc2d
Kyle Robinson Young <kyle@dontkry.com>
2013-02-12 19:36:16 -0800

    Break off test helpers

--- test/tasks/watch_test.js
+++ test/tasks/watch_test.js
@@ -3,97 +3,15 @@
 var grunt = require('grunt');
 var path = require('path');
 var fs = require('fs');
+var helper = require('./helper');
 
-// Where our fixtures are
-var fixtures = path.join(__dirname, '..', 'fixtures');
+var fixtures = helper.fixtures;
 
-// If verbose flag set, display output
-var verboseLog = function() {};
-if (grunt.util._.indexOf(process.argv, '-v') !== -1) {
-  verboseLog = function() { console.log.apply(null, arguments); };
-}
-
-// helper for creating assertTasks for testing tasks in child processes
-function assertTask(task, options) {
-  var spawn = require('child_process').spawn;
-  task = task || 'default';
-  options = options || {};
-
-  // get next/kill process trigger
-  var trigger = options.trigger || '.*(Waiting).*';
-  delete options.trigger;
-
-  // CWD to spawn
-  var cwd = options.cwd || process.cwd();
-  delete options.cwd;
-
-  // Use grunt this process uses
-  var spawnOptions = [process.argv[1]];
-  // Turn options into spawn options
-  grunt.util._.each(options, function(val, key) {
-    spawnOptions.push('--' + key);
-    spawnOptions.push(val);
-  });
-  // Add the tasks to run
-  spawnOptions.push(task);
-
-  // Return an interface for testing this task
-  function returnFunc(runs, done) {
-    // Spawn the node this process uses
-    var spawnGrunt = spawn(process.argv[0], spawnOptions, {cwd:cwd});
-    var out = '';
-
-    if (!grunt.util._.isArray(runs)) {
-      runs = [runs];
-    }
-
-    // Append a last function to kill spawnGrunt
-    runs.push(function() { spawnGrunt.kill('SIGINT'); });
-
-    // After watch starts waiting, run our commands then exit
-    spawnGrunt.stdout.on('data', function(data) {
-      data = grunt.log.uncolor(String(data));
-      out += data;
-
-      // If we should run the next function
-      var shouldRun = true;
-
-      // If our trigger has been found
-      if (trigger !== false) {
-        shouldRun = (new RegExp(trigger, 'gm')).test(data);
-      }
-
-      // Run the function
-      if (shouldRun) {
-        setTimeout(function() {
-          var run = runs.shift();
-          if (typeof run === 'function') { run(); }
-        }, 500);
-      }
-    });
-
-    // Throw errors for better testing
-    spawnGrunt.stderr.on('data', function(data) {
-      throw new Error(data);
-    });
-
-    // On process exit return what has been outputted
-    spawnGrunt.on('exit', function() {
-      done(out);
-    });
-  }
-  returnFunc.options = options;
-  return returnFunc;
-}
-
-// clean up before and after
 function cleanUp() {
-  [
-    path.join(fixtures, 'multiTargets', 'node_modules'),
-    path.join(fixtures, 'oneTarget', 'node_modules'),
-  ].forEach(function(filepath) {
-    if (grunt.file.exists(filepath)) { grunt.file.delete(filepath); }
-  });
+  helper.cleanUp([
+    'multiTargets/node_modules',
+    'oneTarget/node_modules'
+  ]);
 }
 
 exports.watchConfig = {
@@ -110,12 +28,12 @@ exports.watchConfig = {
   oneTarget: function(test) {
     test.expect(2);
     var cwd = path.resolve(fixtures, 'oneTarget');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch(function() {
       var write = 'var test = true;';
       grunt.file.write(path.join(cwd, 'lib', 'one.js'), write);
     }, function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.indexOf('File "lib' + path.sep + 'one.js" changed') !== -1, 'Watch should have fired when oneTarget/lib/one.js has changed.');
       test.ok(result.indexOf('I do absolutely nothing.') !== -1, 'echo task should have fired.');
       test.done();
@@ -124,12 +42,12 @@ exports.watchConfig = {
   multiTargetsTriggerOneNotTwo: function(test) {
     test.expect(2);
     var cwd = path.resolve(fixtures, 'multiTargets');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch(function() {
       var write = 'var test = true;';
       grunt.file.write(path.join(cwd, 'lib', 'one.js'), write);
     }, function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.indexOf('one has changed') !== -1, 'Only task echo:one should of emit.');
       test.ok(result.indexOf('two has changed') === -1, 'Task echo:two should NOT emit.');
       test.done();
@@ -138,13 +56,13 @@ exports.watchConfig = {
   multiTargetsTriggerBoth: function(test) {
     test.expect(2);
     var cwd = path.resolve(fixtures, 'multiTargets');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch([function() {
       grunt.file.write(path.join(cwd, 'lib', 'one.js'), 'var test = true;');
     }, function() {
       grunt.file.write(path.join(cwd, 'lib', 'two.js'), 'var test = true;');
     }], function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.indexOf('one has changed') !== -1, 'Task echo:one should of emit.');
       test.ok(result.indexOf('two has changed') !== -1, 'Task echo:two should of emit.');
       test.done();
@@ -153,14 +71,14 @@ exports.watchConfig = {
   spawnOneAtATime: function(test) {
     test.expect(1);
     var cwd = path.resolve(fixtures, 'multiTargets');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch(function() {
       grunt.file.write(path.join(cwd, 'lib', 'wait.js'), 'var wait = false;');
       setTimeout(function() {
         grunt.file.write(path.join(cwd, 'lib', 'wait.js'), 'var wait = true;');
       }, 500);
     }, function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.indexOf('I waited 2s') !== -1, 'Task should have waited 2s and only spawned once.');
       test.done();
     });
@@ -168,14 +86,14 @@ exports.watchConfig = {
   interrupt: function(test) {
     test.expect(1);
     var cwd = path.resolve(fixtures, 'multiTargets');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch(function() {
       grunt.file.write(path.join(cwd, 'lib', 'interrupt.js'), 'var interrupt = false;');
       setTimeout(function() {
         grunt.file.write(path.join(cwd, 'lib', 'interrupt.js'), 'var interrupt = true;');
       }, 1000);
     }, function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.indexOf('has been interrupted') !== -1, 'Task should have been interrupted.');
       test.done();
     });
@@ -183,11 +101,11 @@ exports.watchConfig = {
   failingTask: function(test) {
     test.expect(2);
     var cwd = path.resolve(fixtures, 'multiTargets');
-    var assertWatch = assertTask('watch', {cwd:cwd});
+    var assertWatch = helper.assertTask('watch', {cwd:cwd});
     assertWatch(function() {
       grunt.file.write(path.join(cwd, 'lib', 'fail.js'), 'var fail = false;');
     }, function(result) {
-      verboseLog(result);
+      helper.verboseLog(result);
       test.ok(result.toLowerCase().indexOf('fatal') !== -1, 'Task should have been fatal.');
       test.equal(grunt.util._(result).count('Waiting...'), 2, 'Should have displayed "Wating..." twice');
       test.done();
