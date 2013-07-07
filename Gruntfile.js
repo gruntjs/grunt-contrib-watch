44edaf2a30b44948d307632f22d9671f82127dea
Kyle Robinson Young <kyle@dontkry.com>
2013-07-07 14:16:25 -0700

    Clean up Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,13 @@
  * grunt-contrib-watch
  * http://gruntjs.com/
  *
- * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
+ * Copyright (c) 2013 "Cowboy" Ben Alman, contributors
  * Licensed under the MIT license.
- * https://github.com/gruntjs/grunt-contrib-watch/blob/master/LICENSE-MIT
  */
 
 'use strict';
 
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
@@ -23,31 +20,23 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
-
-    // Watch
     watch: {
       all: {
         files: ['<%= jshint.all %>'],
         tasks: ['jshint', 'nodeunit'],
       },
     },
-
-    // Unit tests.
     nodeunit: {
       tests: ['test/tasks/*_test.js']
     }
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-internal');
 
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'nodeunit', 'build-contrib']);
-
 };
